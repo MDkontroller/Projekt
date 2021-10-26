5a35ddc26c5deb424adca041cbfd4a7955842434
MDkontroller <43541921+MDkontroller@users.noreply.github.com>
2021-10-26 11:56:24 +0200

    Phidgets Spatial Magnetometer commad shell

---
+++ phidgets_Spatial.sh
@@ -0,0 +1,19 @@
+##
+#   Command shell für die Installation und Inbetriebnahme  Phigets-Spatial Magnetometer
+#-----------------------------------------------------------------------------------------
+
+git clone -b noetic https://github.com/ros-drivers/phidgets_drivers.git
+rosdep install phidgets_drivers             # Install dependencies using rosdep
+cd ..
+catkin_make
+
+# To set up the udev rules for the Phidgets USB devices, run the following commands:
+#------------------------------------------------------------------------------------------
+
+roscd phidgets_api
+sudo cp debian/udev /etc/udev/rules.d/99-phidgets.rules
+sudo udevadm control --reload-rules
+
+#  dmesg 
+
+roslaunch phidgets_spatial spatial.launch
